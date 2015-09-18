95935121a22135b4593d0ce47cd7e00fe0b49f96
sephirothwzc <zhanchao.wu@icloud.com>
2015-09-18 15:35:09 +0800

    测试创建 首次提交

---
+++ Swift_Study/Swift_Study/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Swift_Study
+//
+//  Created by sephiroth on 15/9/18.
+//  Copyright (c) 2015年 FFsephiroth. All rights reserved.
+//
+
+import Foundation
+
+let sj = 10;
+var cj = "2015-09-15";
+
+println("Hello, World!");
+
